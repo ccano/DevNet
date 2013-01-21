6e27acb9aaf5a50e44fed3f15f168ffc2f2cb5c8
PacBio DevNet <devnet@pacificbiosciences.com>
2013-01-21 14:38:34 -0800

    CNAME and bug fix in js

--- javascripts/devnet.js
+++ javascripts/devnet.js
@@ -36,13 +36,13 @@ $(document).ready(function() {
             success: function(){  
               $('.pb-waiting').hide();
               $('.pb-confirm').show();
-              $.cookie('devnetreg', info.email, { expires: (365*10)}); 
+              $.cookie('devnetreg', $('#email').val(), { expires: (365*10)}); 
             },
             error: function(err){ 
               $('.pb-waiting').hide();
               $('.pb-confirm').show(); // submission works, jsonp bonks; suppress
               //$('.pb-error').show();
-              $.cookie('devnetreg', info.email, { expires: (365*10)}); 
+              $.cookie('devnetreg', $('#email').val(), { expires: (365*10)}); 
             }
           });
           return false;
